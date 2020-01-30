96056ee3bcbd2fe3f1305845061a521bb342fa31
Marios Nicolaides <mnico005@ucr.edu>
2020-01-30 13:48:59 -0800

    Update alu_controltb.v

--- alu_controltb.v
+++ alu_controltb.v
@@ -4,15 +4,15 @@
 // Company: 
 // Engineer:
 //
-// Create Date:   21:22:09 01/30/2020
-// Design Name:   control_unit
-// Module Name:   /home/ise/Desktop/CS161L/lab3/control_unittb.v
+// Create Date:   21:21:46 01/30/2020
+// Design Name:   alu_control
+// Module Name:   /home/ise/Desktop/CS161L/lab3/alu_controltb.v
 // Project Name:  lab3
 // Target Device:  
 // Tool versions:  
 // Description: 
 //
-// Verilog Test Fixture created by ISE for module: control_unit
+// Verilog Test Fixture created by ISE for module: alu_control
 //
 // Dependencies:
 // 
@@ -22,37 +22,26 @@
 // 
 ////////////////////////////////////////////////////////////////////////////////
 
-module control_unittb;
+module alu_controltb;
 
 	// Inputs
-	reg [5:0] instr_op;
+	reg [1:0] alu_op;
+	reg [5:0] instruction_5_0;
 
 	// Outputs
-	wire reg_dst;
-	wire branch;
-	wire mem_read;
-	wire mem_to_reg;
-	wire [1:0] alu_op;
-	wire mem_write;
-	wire alu_src;
-	wire reg_write;
+	wire [3:0] alu_out;
 
 	// Instantiate the Unit Under Test (UUT)
-	control_unit uut (
-		.instr_op(instr_op), 
-		.reg_dst(reg_dst), 
-		.branch(branch), 
-		.mem_read(mem_read), 
-		.mem_to_reg(mem_to_reg), 
+	alu_control uut (
 		.alu_op(alu_op), 
-		.mem_write(mem_write), 
-		.alu_src(alu_src), 
-		.reg_write(reg_write)
+		.instruction_5_0(instruction_5_0), 
+		.alu_out(alu_out)
 	);
 
 	initial begin
 		// Initialize Inputs
-		instr_op = 0;
+		alu_op = 0;
+		instruction_5_0 = 0;
 
 		// Wait 100 ns for global reset to finish
 		#100;
@@ -62,9 +51,9 @@ module control_unittb;
             #5 clk = ~clk;
       end
 
-
 	end
-   integer totalTests = 0;
+	
+	integer totalTests = 0;
     integer failedTests = 0;
     initial begin
         // Initialize inputs
@@ -79,61 +68,109 @@ module control_unittb;
 		  // ---------------------------------------------
 		  // Testing unsigned additions 
 		  // --------------------------------------------- 
-		  instr_op = 6'b000000 ; 
+		  alu_op = 2'b00 ; 
         $write("Test case 1: <Description>...");
         totalTests = totalTests + 1; 
-		
+		  instruction_5_0 = 6'b000000
         #10;
         // Check if result != expected
-        if (alu_op != 2'b10 || branch != 0 || mem_read != 0 || mem_write != 0 || reg_write!= 1 || mem_to_reg != 0 || alu_src != 0 || reg_dst != 1) begin 
+        if (alu_out ! = 4'b0010) begin 
             $display("...failed");
             failedTests = failedTests + 1;
         end else begin
             $display("...passed");
         end   
 		  
-		  instr_op = 6'b110001 ; 
+		  alu_op = 2'b01 ; 
         $write("Test case 2: <Description>...");
         totalTests = totalTests + 1; 
-		
+		  instruction_5_0 = 6'b000000
         #10;
         // Check if result != expected
-        if (alu_op != 2'b00 || branch != 0 || mem_read != 1 || mem_write != 0 || reg_write!= 1 || mem_to_reg != 1 || alu_src != 1 || reg_dst != 0) begin 
+        if (alu_out ! = 4'b0110) begin 
             $display("...failed");
             failedTests = failedTests + 1;
         end else begin
             $display("...passed");
         end 
 		  
-		  instr_op = 6'b110101 ; 
+		  alu_op = 2'b10 ; 
         $write("Test case 3: <Description>...");
         totalTests = totalTests + 1; 
-		
+		  instruction_5_0 = 6'b100000
         #10;
         // Check if result != expected
-        if (alu_op != 2'b00 || branch != 0 || mem_read != 0 || mem_write != 1 || reg_write!= 0  || alu_src != 1 ) begin 
+        if (alu_out ! = 4'b0010) begin 
             $display("...failed");
             failedTests = failedTests + 1;
         end else begin
             $display("...passed");
-        end 
+        end
 		  
-		  instr_op = 6'b001000 ; 
-        $write("Test case 4: <Description>...");
+		   $write("Test case 4: <Description>...");
         totalTests = totalTests + 1; 
-		
+		  instruction_5_0 = 6'b100010
         #10;
         // Check if result != expected
-        if (alu_op != 2'b01 || branch != 1 || mem_read != 0 || mem_write != 0 || reg_write!= 0  || alu_src != 0 ) begin 
+        if (alu_out ! = 4'b0110) begin 
             $display("...failed");
             failedTests = failedTests + 1;
         end else begin
             $display("...passed");
-        end 
+        end
+		  
+		  $write("Test case 5: <Description>...");
+        totalTests = totalTests + 1; 
+		  instruction_5_0 = 6'b100100
+        #10;
+        // Check if result != expected
+        if (alu_out ! = 4'b0000) begin 
+            $display("...failed");
+            failedTests = failedTests + 1;
+        end else begin
+            $display("...passed");
+        end
+		  
+		  $write("Test case 6: <Description>...");
+        totalTests = totalTests + 1; 
+		  instruction_5_0 = 6'b100101
+        #10;
+        // Check if result != expected
+        if (alu_out ! = 4'b0001) begin 
+            $display("...failed");
+            failedTests = failedTests + 1;
+        end else begin
+            $display("...passed");
+        end
+		  
+		  $write("Test case 7: <Description>...");
+        totalTests = totalTests + 1; 
+		  instruction_5_0 = 6'b101010
+        #10;
+        // Check if result != expected
+        if (alu_out ! = 4'b0111) begin 
+            $display("...failed");
+            failedTests = failedTests + 1;
+        end else begin
+            $display("...passed");
+        end
+		  
+		  $write("Test case 8: <Description>...");
+        totalTests = totalTests + 1; 
+		  instruction_5_0 = 6'b100111
+        #10;
+        // Check if result != expected
+        if (alu_out ! = 4'b1100) begin 
+            $display("...failed");
+            failedTests = failedTests + 1;
+        end else begin
+            $display("...passed");
+        end
 		  
 		  $display("\n-----------------------------------------");
         $display("\nTesting complete\nPassed %d/%d tests", totalTests-failedTests,totalTests);
         $display("\n-----------------------------------------");
 	end 
+      
 endmodule
 
